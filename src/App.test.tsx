4534e43c43b2d8ba20dadd7560211a1f617f652e
Pavel Levchenko <plevchenko@star.global>
2021-10-29 12:09:43 +0300

    comment unused code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+// import { Provider } from 'react-redux';
+// import { store } from './app/store';
 import App from './App';
 
 test('render hello world', ()=>{
