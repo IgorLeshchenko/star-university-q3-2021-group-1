6cf7b0bcbdd5937131ecc60c4a3a0c7c89e4b627
Pavel Levchenko <plevchenko@star.global>
2021-10-29 16:00:26 +0300

    add prittier config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 // import { Provider } from 'react-redux';
 // import { store } from './app/store';
-import App from './App';
+import App from "./App";
 
-test('render hello world', ()=>{
-    render(<App />);
-    const helloWorldElement = screen.getByText('Hello World from group 1!!!')
-    expect(helloWorldElement).toBeInTheDocument()
-})
-
-
-
+test("render hello world", () => {
+  render(<App />);
+  const helloWorldElement = screen.getByText("Hello World from group 1!!!");
+  expect(helloWorldElement).toBeInTheDocument();
+});
 
 // test('renders learn react link', () => {
 //   const { getByText } = render(
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      
-        <p>
-         Hello World from group 1!!!
-        </p>
-        
+
+        <p>Hello World from group 1!!!</p>
       </header>
     </div>
   );
