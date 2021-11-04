22bdb4f1860abfefd8ded635b56a4833407967df
vkravchuk2 <90546973+vkravchuk2@users.noreply.github.com>
2021-11-04 14:38:33 +0200

    Update src/components/Coments/Coment.tsx
    Co-authored-by: Iryna Lian <45939370+irenlian@users.noreply.github.com>

--- src/components/Coments/Coment.tsx
+++ src/components/Coments/Coment.tsx
@@ -6,12 +6,14 @@ import { deepOrange } from "@mui/material/colors"
 const Coment: React.FC<{ items: string[] }> = (props) => {
     return <div className='wrapperComent'>
         <div className='comentMessage'>
-            {props.items.map((item, i) => (<div className='comentItem'>
+            {props.items.map((item, i) => (
+            <div className='comentItem' key={item.id} >
                 <div className='userAvatar'>
                     <Avatar src='' alt='User avatar' sx={{ width: 20, height: 20, bgcolor: deepOrange[500] }} />
                     <p className='nickname'>User nickname</p>
                 </div>
-                <TextField multiline key={item[i]} margin='dense' disabled variant='outlined' defaultValue={props.items[i]} ></TextField></div>
+                <TextField multiline margin='dense' disabled variant='outlined' defaultValue={item} ></TextField>
+                </div>
             ))}
 
         </div>
