abb09486bfd2517e677ca96df213fc1f6286bb9f
Anna Ostrovska <aostrovska@star.global>
2021-11-11 23:36:50 +0200

    change types

--- src/screens/main/sort/SortByTopButton.tsx
+++ src/screens/main/sort/SortByTopButton.tsx
@@ -1,47 +1,31 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "@material-ui/core";
 import { useStyles } from "../style";
-import axios from 'axios';
-
-interface Post {
-  author: string,
-  body: string,
-  children: Post[],
-  date: string,
-  level: number,
-  parent: string,
-  title: string,
-  upvotes: number,
-  __v?: number,
-  _id: string,
-}
+import axios from "axios";
+import { IPost } from "../../../components/post/types";
 
 interface Props {
   sortedPosts(srtdPosts: []): void;
 }
 
-
-const SortByTopButton: React.FC<Props> = ({sortedPosts}) => {
+const SortByTopButton: React.FC<Props> = ({ sortedPosts }) => {
   const { button } = useStyles();
 
   const [posts, setPosts] = useState([]);
 
   const getPosts = async () => {
-    const response= await axios.get('https://starforum.herokuapp.com/api/v1/posts');
+    const response = await axios.get("https://starforum.herokuapp.com/api/v1/posts");
     setPosts(response.data);
-  }
+  };
 
   const sortByTop = () => {
     getPosts();
   };
 
   useEffect(() => {
-    const sortedByTop = posts.sort((a: Post, b: Post) => b.upvotes - a.upvotes);
-    console.log(sortedByTop);
+    const sortedByTop = posts.sort((a: IPost, b: IPost) => b.upvotes - a.upvotes);
     sortedPosts(sortedByTop as []);
   }, [posts]);
-
-
 
   return (
     <div>
