310b39ff297b941c9c52859875112dae595ae614
Anna Ostrovska <aostrovska@star.global>
2021-11-12 16:45:32 +0200

    add useState

--- src/screens/main/MainScreen.tsx
+++ src/screens/main/MainScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Box, Button, TextField, Typography } from "@material-ui/core";
 import { useSelector, useDispatch } from "react-redux";
