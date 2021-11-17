ac8149f4a01076d887b2c37a4f8d0797c6ad6262
Anna Ostrovska <aostrovska@star.global>
2021-11-17 16:28:34 +0200

    add sorting from server

--- src/screens/main/components/SortByNewButton.tsx
+++ src/screens/main/components/SortByNewButton.tsx
@@ -6,26 +6,26 @@ import { useDispatch } from "react-redux";
 import { postsAction } from "../../../app/store/postsSlice";
 
 interface Props {
-    sortedPosts(srtdPosts: []): void;
+  sortedPosts(srtdPosts: []): void;
 }
 
 const SortByNewButton: React.FC<Props> = ({ sortedPosts }) => {
-    const { button } = useStyles();
-    const dispatch = useDispatch();
+  const { button } = useStyles();
+  const dispatch = useDispatch();
 
-    const sortByNew = () => {
-        fetch("https://starforum.herokuapp.com/api/v1/posts?sort=recent")
-            .then(response => response.json())
-            .then(json => dispatch(postsAction.setPosts(json)));
-    };
+  const sortByNew = () => {
+    fetch("https://starforum.herokuapp.com/api/v1/posts?sort=recent")
+      .then(response => response.json())
+      .then(json => dispatch(postsAction.setPosts(json)));
+  };
 
-    return (
-        <div>
-            <Button variant="outlined" className={button} onClick={sortByNew}>
-                NEW
-            </Button>
-        </div>
-    );
+  return (
+    <div>
+      <Button variant="outlined" className={button} onClick={sortByNew}>
+        NEW
+      </Button>
+    </div>
+  );
 };
 
 export default SortByNewButton;
