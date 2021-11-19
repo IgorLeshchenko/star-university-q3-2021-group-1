cfab2a3ad27db80f8fdedc71fa5094d7cb242d50
Vkravchuk <vkravchuk@star.global>
2021-11-19 13:46:40 +0200

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {HashRouter as BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter as BrowserRouter, Route, Switch } from "react-router-dom";
 
 import LoginScreen from "./screens/login";
 import MainScreen from "./screens/main";
